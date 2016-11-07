674a06c69fbc3860f1dec86c926614096fc2e602
Gabriel Silva Tosetti <Gabriel Silva Tosetti>
2016-11-07 14:09:31 -0200

    Configuração do formulario de login

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -2,5 +2,8 @@
 
 class LoginController extends \HXPHP\System\Controller
 {
-	
+	public function logarAction()
+	{
+		$this->view->setFile('index'); //quando chamada esta ação, ela aproveita a view Index.
+	}
 }
