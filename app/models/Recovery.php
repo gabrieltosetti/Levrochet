e029248a4b39e936fd30a29368d2b21bb2deeb61
Gabriel Silva Tosetti <Gabriel Silva Tosetti>
2016-11-07 23:42:03 -0200

    validação da existencia do usuário para gerar o link de redefinição

---
+++ app/models/Recovery.php
@@ -0,0 +1,33 @@
+<?php 
+
+class Recovery extends \HXPHP\System\Model
+{
+	public static function validar($user_email)
+	{
+		$callback_obj = new \stdClass;
+		$callback_obj->user = null;
+		$callback_obj->code = null;
+		$callback_obj->status = false;
+
+		$user_exists = User::find_by_email($user_email);
+
+		if( !is_null($user_exists))
+		{
+			$callback_obj->status = true;
+			$callback_obj->user = $user_exists;
+
+			self::delete_all(array(
+				'conditions' => array(
+					'user_id = ?',
+					$user_exists->id
+				)
+			));
+		}
+		else
+		{
+			$callback_obj->code = 'nenhum-usuario-encontrado';
+		}
+
+		return $callback_obj;
+	}
+}
